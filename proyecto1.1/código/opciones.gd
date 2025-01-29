0f0c25527ef28093c68454e9d4f07ff3b21f0c0c
AlejandroF-cmd <4144809@alu.murciaeduca.es>
2025-01-29 13:28:47 +0100

    Add files via upload

---
+++ proyecto1.1/código/opciones.gd
@@ -0,0 +1,33 @@
+extends Node2D
+var change_scene2 = load("res://escenas/Instrucciones.tscn")
+var change_scene3 = load("res://escenas/Volumen.tscn")
+var change_scene4 = load("res://escenas/Créditos.tscn")
+var change_scene1 = load("res://escenas/menú_p.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_instrucciones_pressed() -> void:
+	get_tree().change_scene_to_packed(change_scene2)
+	pass # Replace with function body.
+
+func _on_volumen_pressed() -> void:
+	get_tree().change_scene_to_packed(change_scene3)
+	pass # Replace with function body.
+
+func _on_créditos_pressed() -> void:
+	get_tree().change_scene_to_packed(change_scene4)
+	pass # Replace with function body.
+
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_packed(change_scene1)
+	pass # Replace with function body.
