2fe9547eec53c9094f659109d65e7013a74df3c0
AlejandroF-cmd <4144809@alu.murciaeduca.es>
2025-03-04 14:24:08 +0100

    Add files via upload

---
+++ proyecto1.2.1/código/jugar.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var change_scene = load("res://escenas/menú_p.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_packed(change_scene)
+	pass # Replace with function body.
